8f1a7659c3c1f16c5021bbca31a0dd59c9c30650
RayanMendis56 <rayanmendisa13@gmail.com>
2026-04-24 13:57:04 +0530

    Add Javascript to make stopwatch interactive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
 console.log("JS loaded");
+
+let [seconds,minutes,hours]=[0,0,0];
+let displayTime=document.getElementById("displayTime");
+let timer=null;
+let startBtn=document.getElementById("startBtn");
+
+function stopwatch(){
+    seconds++;
+    if (seconds==60){
+        seconds=0;
+        minutes++;
+        if(minutes==60){
+            minutes=0;
+            hours++;
+        }
+    }
+    let h=hours<10 ? "0"+hours:hours;
+    let m=minutes<10 ? "0"+minutes:minutes;
+    let s=seconds<10 ? "0"+seconds:seconds;
+
+
+    displayTime.innerHTML=h +":"+ m + ":"+ s;
+}
+
+function watchstart(){
+    if(timer!=null){
+        clearInterval(timer);
+        timer=null;
+        startBtn.src="../images/start.png"
+    }
+    else{
+        timer=setInterval(stopwatch,1000);
+        startBtn.src="../images/pause.png";
+    }
+    
+}
+
+function watchstop(){
+    clearInterval(timer);
+}
+
+function watchreset(){
+    clearInterval(timer);
+    [seconds,minutes,hours]=[0,0,0];
+    displayTime.innerHTML="00:00:00";
+    startBtn.src="../images/start.png";
+}
